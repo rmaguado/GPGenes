d362c4e5bc1a2a1c8e616cce8f4c8d10c94419c5
rmaguado <ruben.morenoaguado@gmail.com>
2026-01-17 14:50:42 +0000

    increase test complexity

--- gpgenes/data/test.py
+++ gpgenes/data/test.py
@@ -3,10 +3,10 @@ from . import create_genes, plot_graph
 if __name__ == "__main__":
 
     genes = create_genes(
-        n_genes=10,
-        n_sparse=2,
-        n_motif=8,
-        seed=1,
+        n_genes=20,
+        n_sparse=3,
+        n_motif=17,
+        seed=0,
     )
 
     plot_graph(genes)
